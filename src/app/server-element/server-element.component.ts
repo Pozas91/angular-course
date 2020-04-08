ca444df9dd3983d44a08a6d173ad91d45a8b03a1
Pozas91 <pozas91@gmail.com>
2020-04-08 16:38:37 +0200

    Understanding the Component Lifecycle Seeing Lifecycle Hooks in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -13,15 +25,45 @@ import {Component, Input, OnInit} from '@angular/core';
   // Use Shadow DOM to encapsulate styles
   // encapsulation: ViewEncapsulation.ShadowDom
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   // By default all components are private (only available inside the class), so we use @Input()
   // We use an alias to use this property from output.
   @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
   constructor() {
+    console.log('ServerElement: constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ServerElement: ngOnChanges called!', changes);
   }
 
   ngOnInit(): void {
+    console.log('ServerElement: ngOnInit called!');
   }
 
+  ngDoCheck(): void {
+    console.log('ServerElement: ngDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ServerElement: ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ServerElement: ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ServerElement: ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ServerElement: ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ServerElement: ngOnDestroy called!');
+  }
 }
