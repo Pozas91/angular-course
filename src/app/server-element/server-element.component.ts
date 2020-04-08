9a77141635ca67fcc93f981da8cb29cf5e4d98f3
Pozas91 <pozas91@gmail.com>
2020-04-08 12:32:01 +0200

    More on View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -3,7 +3,15 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // Avoid View Encapsulation
+  // encapsulation: ViewEncapsulation.None
+
+  // Default option
+  // encapsulation: ViewEncapsulation.Emulated
+
+  // Use Shadow DOM to encapsulate styles
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent implements OnInit {
   // By default all components are private (only available inside the class), so we use @Input()
