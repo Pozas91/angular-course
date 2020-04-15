ec7ba68fde5b604d4617abf885f81df6e44632cb
Pozas91 <pozas91@gmail.com>
2020-04-15 17:30:24 +0200

    Using Types with the HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +18,10 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://curso-angular-4aa37.firebaseio.com/posts.json',
         postData
       )
@@ -40,9 +41,9 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get('https://curso-angular-4aa37.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postsArray = [];
+      .get<{ [key: string]: Post }>('https://curso-angular-4aa37.firebaseio.com/posts.json')
+      .pipe(map((responseData) => {
+        const postsArray: Post[] = [];
 
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
