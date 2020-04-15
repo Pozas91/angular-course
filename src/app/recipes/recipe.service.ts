9fa23be0c8311c2e9bd600db55a9f386809db1bd
Pozas91 <pozas91@gmail.com>
2020-04-15 22:02:59 +0200

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,27 +8,29 @@ import {Subject} from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Skewer', 'This is simply a test',
-      'https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Pepper', 1),
-        new Ingredient('Onion', 1),
-        new Ingredient('Cheese', 1)
-      ]
-    ),
-    new Recipe(
-      'Ribs',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Skewer', 'This is simply a test',
+  //     'https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Pepper', 1),
+  //       new Ingredient('Onion', 1),
+  //       new Ingredient('Cheese', 1)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Ribs',
+  //     'This is simply a test',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]
+  //   )
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {
   }
