d3a189a59fd149278bd557f6a4143a4042495107
Pozas91 <pozas91@gmail.com>
2020-04-15 18:15:30 +0200

    Setting Headers

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Post} from './post.model';
 import {catchError, map} from 'rxjs/operators';
 import {Subject, throwError} from 'rxjs';
@@ -30,7 +30,12 @@ export class PostsService {
 
   fetchPosts() {
     return this.http
-      .get<{ [key: string]: Post }>('https://curso-angular-4aa37.firebaseio.com/posts.json')
+      .get<{ [key: string]: Post }>(
+        'https://curso-angular-4aa37.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+        }
+      )
       .pipe(map((responseData) => {
           const postsArray: Post[] = [];
 
