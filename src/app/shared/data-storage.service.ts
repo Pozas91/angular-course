7c357c959319c39eed5b2169cdd9982678438183
Pozas91 <pozas91@gmail.com>
2020-04-15 23:00:28 +0200

    Fixing a Bug with the Resolver

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -31,9 +31,6 @@ export class DataStorageService {
         tap(recipes => {
           this.recipeService.setRecipes(recipes);
         })
-      )
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-      });
+      );
   }
 }
