dfbf7fbf10c662251aa86e347f491c7f48c322af
Pozas91 <pozas91@gmail.com>
2020-04-16 12:22:28 +0200

    Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
+import {User} from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -17,6 +18,8 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {
   }
 
@@ -28,7 +31,12 @@ export class AuthService {
         password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(
+      catchError(this.handleError),
+      tap(responseData => {
+        this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+      })
+    );
   }
 
   signIn(email: string, password: string) {
@@ -40,6 +48,12 @@ export class AuthService {
         returnSecureToken: true
       }
     ).pipe(catchError(this.handleError));
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
