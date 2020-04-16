ccd4930bc0f9873d8fbf84735d3228e9e1a24744
Pozas91 <pozas91@gmail.com>
2020-04-16 13:38:42 +0200

    Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
-import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import {AuthService} from '../auth/auth.service';
 
 @Injectable({
@@ -24,29 +24,23 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http
-          .get<Recipe[]>(
-            'https://curso-angular-4aa37.firebaseio.com/recipes.json',
-            {
-              params: new HttpParams().set('auth', user.token)
+
+    return this.http
+      .get<Recipe[]>(
+        'https://curso-angular-4aa37.firebaseio.com/recipes.json',
+      ).pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+              return {
+                ...recipe,
+                ingredients: recipe.ingredients ? recipe.ingredients : []
+              };
             }
           );
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          }
-        );
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
