ca6bac6e752611bde0d4b5d81d8b5fa1ab006557
Pozas91 <pozas91@gmail.com>
2020-04-17 10:50:00 +0200

    More Lazy Loading

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {AuthComponent} from './auth.component';
 import {CommonModule} from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
@@ -12,10 +11,9 @@ import {SharedModule} from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    BrowserModule,
     FormsModule,
     RouterModule.forChild([
-      {path: 'auth', component: AuthComponent}
+      {path: '', component: AuthComponent}
     ]),
     SharedModule
   ]
