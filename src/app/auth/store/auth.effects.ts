ad1326138d2bb71dd2d4dcaf0da06d90a5e692b1
Pozas91 <pozas91@gmail.com>
2020-04-18 11:11:32 +0200

    Adding Signup

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -9,12 +9,64 @@ import {of} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 
+const handleAuthentication = (
+  expiresIn: number,
+  email: string,
+  userId: string,
+  token: string
+) => {
+  const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+  return new AuthActions.AuthenticateSuccess({
+    email, userId, token, expirationDate
+  });
+};
+
+const handlerError = (errorRes: any) => {
+  let errorMessage = 'Oops! Something go wrong';
+
+  if (!errorRes.error || !errorRes.error.error) {
+    return of(new AuthActions.AuthenticateFail(errorMessage));
+  }
+
+  switch (errorRes.error.error.message) {
+    case 'EMAIL_EXISTS':
+      errorMessage = 'This email already exists';
+      break;
+    case 'EMAIL_NOT_FOUND':
+      errorMessage = 'This email does not exist';
+      break;
+    case 'INVALID_PASSWORD':
+      errorMessage = 'This password is not correct';
+      break;
+  }
+
+  return of(new AuthActions.AuthenticateFail(errorMessage));
+};
+
 @Injectable()
 export class AuthEffects {
 
   @Effect()
   authSignUp = this.actions$.pipe(
-    ofType(AuthActions.SIGN_UP_START)
+    ofType(AuthActions.SIGN_UP_START),
+    switchMap((signUpAction: AuthActions.SignUpStart) => {
+      return this.http.post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
+        {
+          email: signUpAction.payload.email,
+          password: signUpAction.payload.password,
+          returnSecureToken: true
+        }
+      ).pipe(
+        // Success Handling
+        map(resData => {
+          return handleAuthentication(+resData.expiresIn, resData.email, resData.localId, resData.idToken);
+        }),
+        // Error Handling
+        catchError(errorRes => handlerError(errorRes))
+      );
+    })
   );
 
   @Effect()
@@ -31,35 +83,10 @@ export class AuthEffects {
       ).pipe(
         // Success Handling
         map(resData => {
-          const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-
-          return new AuthActions.AuthenticateSuccess({
-            email: resData.email, userId: resData.localId, token: resData.idToken, expirationDate
-          });
+          return handleAuthentication(+resData.expiresIn, resData.email, resData.localId, resData.idToken);
         }),
         // Error Handling
-        catchError(errorRes => {
-
-          let errorMessage = 'Oops! Something go wrong';
-
-          if (!errorRes.error || !errorRes.error.error) {
-            return of(new AuthActions.AuthenticateFail(errorMessage));
-          }
-
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email already exists';
-              break;
-            case 'EMAIL_NOT_FOUND':
-              errorMessage = 'This email does not exist';
-              break;
-            case 'INVALID_PASSWORD':
-              errorMessage = 'This password is not correct';
-              break;
-          }
-
-          return of(new AuthActions.AuthenticateFail(errorMessage));
-        }),
+        catchError(errorRes => handlerError(errorRes)),
       );
     }),
   );
